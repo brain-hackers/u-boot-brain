94e91aa4d57c0b298ee45babde720d24e0f3a08a
Suguru Saito <sg.sgch07@gmail.com>
2021-05-29 23:12:38 +0900

    sharp: fix implicit declaration

--- board/sharp/common/dma.c
+++ board/sharp/common/dma.c
@@ -1,6 +1,7 @@
 #include <common.h>
 #include <bootm.h>
 #include <asm/mach-imx/dma.h>
+#include <asm/arch/sys_proto.h>
 
 void board_quiesce_devices(void) {
 	mxs_dma_disable(MXS_DMA_CHANNEL_AHB_APBH_LCDIF);
